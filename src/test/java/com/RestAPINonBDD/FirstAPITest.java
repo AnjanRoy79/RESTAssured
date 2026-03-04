28959ad7d5afe5549a625d1a1ae1d00df1a2ac7f
AnjanRoy79 <anjanroycal@gmail.com>
2026-03-04 19:48:00 +0530

    Api Key added and code modified

--- src/test/java/com/RestAPINonBDD/FirstAPITest.java
+++ src/test/java/com/RestAPINonBDD/FirstAPITest.java
@@ -4,31 +4,31 @@ import java.util.concurrent.TimeUnit;
 import org.testng.annotations.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 public class FirstAPITest {
-  @Test
-  
-  public void testGETSingleUser()
-  {
-	  /*
-	   * RestAssured is class and Response is an interface
-	   */
-	  
-	  //send the request
-	  Response res=RestAssured.get("https://reqres.in/api/users/2");
-	  
-	  //get the Response
-	  System.out.println("Response code is: "+res.getStatusCode()); // getting response status code
-	  System.out.println("REsponse stataus line: "+res.getStatusLine()); // getting response status message
-	  System.out.println("REsponse Time in ms: "+res.getTimeIn(TimeUnit.MILLISECONDS));
-	  // getting response time in milli sec
-	  
-	  System.out.println("REsponse in text format:RAW type");
-	  System.out.println(res.asString()); // getting response body/JSON in raw type or text
-	  
-	  System.out.println("Response in JSON");
-	  System.out.println(res.asPrettyString()); // getting response body/JSON as JSON string format
-	   
-	  
-  }
+
+    @Test
+    public void testGETWithAuth() {
+
+        // Base URI (ONLY domain)
+        RestAssured.baseURI = "https://reqres.in";
+
+        // Create request specification
+        RequestSpecification request = RestAssured.given();
+
+        // Add API Key header
+        request.header("x-api-key", "reqres_dce1220dd31f4daf9149d6396d70f5bc");
+
+        //  Endpoint
+        Response res = request.get("/api/users/2");
+
+        // Print response
+        System.out.println("Status Code: " + res.getStatusCode());
+        System.out.println("Status Line: " + res.getStatusLine());
+        System.out.println("Response Time: " +
+                res.getTimeIn(TimeUnit.MILLISECONDS));
+
+        System.out.println(res.asPrettyString());
+    }
 }
--- src/test/java/com/RestAPINonBDD/SecondAPITestWithValidation.java
+++ src/test/java/com/RestAPINonBDD/SecondAPITestWithValidation.java
@@ -5,12 +5,15 @@ import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 public class SecondAPITestWithValidation {
   @Test
   public void testGETSingleUserWithValidation() 
   {
 	  Response res=RestAssured.get("https://reqres.in/api/users/2");
+	  RequestSpecification request = RestAssured.given();
+	  request.header("x-api-key", "reqres_dce1220dd31f4daf9149d6396d70f5bc");
 	  
 	  int expstatuscode=403;
 	  int actstatuscode=res.getStatusCode();
